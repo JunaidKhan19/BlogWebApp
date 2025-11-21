1c83b47063969cccab90adfe62d1747c1a061eda
JunaidKhan19 <khanjunaid1719@gmail.com>
2025-11-21 12:01:46 +0530

    Seeding admin role for authorization

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace BlogWebApplication
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -38,6 +38,50 @@ namespace BlogWebApplication
             });
 
             var app = builder.Build();
+
+            //making scope for seeding roles and admin user
+            using (var scope = app.Services.CreateScope())
+            {
+                //accessing services
+                var _userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var _roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                //creating roles and admin user
+                string adminEmail = "admin@gmail.com";
+                string adminPassword = "Admin@123";
+
+                //check if admin role exists
+                var existingAdmin = await _roleManager.FindByNameAsync("Admin");
+
+                //if not, create it
+                if (existingAdmin == null)
+                {
+                    //create admin role
+                    await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                }
+
+                //check if admin user exists
+                var existingUser = await _userManager.FindByEmailAsync(adminEmail);
+
+                //if not, create it
+                if (existingUser == null)
+                {
+                    //create admin user
+                    var newAdminUser = new IdentityUser
+                    {
+                        UserName = adminEmail,
+                        Email = adminEmail,
+                        EmailConfirmed = true
+                    };
+                    var result = await _userManager.CreateAsync(newAdminUser, adminPassword);
+
+                    //if user creation is successful, assign admin role
+                    if (result.Succeeded)
+                    {
+                        await _userManager.AddToRoleAsync(newAdminUser, "Admin");
+                    }
+                }
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
